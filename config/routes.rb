7930056f0568d39460bf76ac5d958300bb55d516
Jesus Zuñiga <jesuszt@hotmail.com>
2022-01-21 12:58:07 -0600

    Modificaciones 21/01/2022

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :friends
   #get 'home/index'
   root "home#index"
@@ -7,4 +8,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
 end
